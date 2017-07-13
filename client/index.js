02aa367a13e221d97026ed2597280bdb712d2745
Namelos <namelos@outlook.com>
2017-07-14 00:03:04 +0800

    add react router

--- client/index.js
+++ client/index.js
@@ -1,4 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-render(<p>React.</p>, document.querySelector('#app'))
+const Home = () => <div>Home</div>
+
+const App = () => <BrowserRouter>
+  <div>
+    <ul>
+      <li><Link to="/">Index</Link></li>
+      <li><Link to="/home">Home</Link></li>
+    </ul>
+
+    <hr/>
+
+    <Route path="/home" component={Home}/>
+  </div>
+</BrowserRouter>
+
+render(<App/>, document.querySelector('#app'))
